d8750ce63b3110bc22f467900e73ec66c43bd363
Diego Osuna <7rgsx7d6jz@privaterelay.appleid.com>
2026-01-27 02:02:51 +0000

    random card generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  const card = ["♠", "♦", "♣", "♥"].flatMap(s => ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"].map(r => ({ r, s }))).sort(() => Math.random() - .5).pop();
+  document.querySelector("#card-container").innerHTML = `<div class="playing-card shadow ${"♥♦".includes(card.s) ? 'text-danger' : 'text-dark'}"><div class="card-corner top-left"><div>${card.r}</div><div>${card.s}</div></div><div class="card-center">${card.s}</div><div class="card-corner bottom-right"><div>${card.r}</div><div>${card.s}</div></div></div>`;
 };
